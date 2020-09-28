3a00a63d334c8ef655ec05ba92a412f485d9fe58
RYAZANTSEV <1mo78@mail.ru>
2020-09-28 18:27:41 +0400

    1.1. Входные данные в тесте [#373236]

--- src/ru/job4j/converter/ConverterTest.java
+++ src/ru/job4j/converter/ConverterTest.java
@@ -6,31 +6,31 @@ import org.junit.Assert;
 public class ConverterTest {
 
     @Test
-    public void whenConvert140rubThen2Euro{
+    public void whenConvert140rubThen2Euro() {
         int in = 140;
         int expected = 2;
-        int out = Converter.rubleToEuro(in;
+        int out = Converter.rubleToEuro(in);
         Assert.assertEquals(expected, out);
     }
 
     @Test
-    public void whenConvert140rubThen2Dollar{
+    public void whenConvert140rubThen2Dollar() {
         int in = 140;
         int expected = 2;
-        int out = Converter.rubleToDollar(in)in;
+        int out = Converter.rubleToDollar(in);
         Assert.assertEquals(expected, out);
     }
 
     @Test
-    public void whenConvert140DollarThen8400Ruble{
+    public void whenConvert140DollarThen8400Ruble() {
         int in = 140;
         int expected = 8400;
-        int out = Converter.rubleToDollar(in)in;
+        int out = Converter.rubleToDollar(in);
         Assert.assertEquals(expected, out);
     }
 
     @Test
-    public void whenConvert140EuroThen9800Ruble{
+    public void whenConvert140EuroThen9800Ruble() {
         int in = 140;
         int expected = 9800;
         int out = Converter.euroToRuble(in);
